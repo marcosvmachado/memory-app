6ca00b4585dc4c38deb148aa0919f30f9cdd82f8
marcosdu22 <marcosviniciusdu22@gmail.com>
2025-12-29 22:40:10 -0400

    quase acabando

--- app/page.tsx
+++ app/page.tsx
@@ -14,18 +14,19 @@ type Item = {
 const Home = () => {
   
   const [items, setItems] = useState<Item[]>([
-    {id: 0, defaultIcon:"/assets/b7.svg", icon:"/assets/android.svg", toShow: false, permanentShow: false},
-    {id: 1, defaultIcon:"/assets/b7.svg", icon:"/assets/android.svg", toShow: false, permanentShow: false},
-    {id: 2, defaultIcon:"/assets/b7.svg", icon:"/assets/caminhao.svg", toShow: false, permanentShow: false},
-    {id: 3, defaultIcon:"/assets/b7.svg", icon:"/assets/caminhao.svg", toShow: false, permanentShow: false},
-    {id: 4, defaultIcon:"/assets/b7.svg", icon:"/assets/disney.svg", toShow: false, permanentShow: false},
-    {id: 5, defaultIcon:"/assets/b7.svg", icon:"/assets/disney.svg", toShow: false, permanentShow: false},
-    {id: 6, defaultIcon:"/assets/b7.svg", icon:"/assets/estrela.svg", toShow: false, permanentShow: false},
-    {id: 7, defaultIcon:"/assets/b7.svg", icon:"/assets/estrela.svg", toShow: false, permanentShow: false},
+    {id: 0, defaultIcon:"/assets/b7.svg", icon:"/assets/moto.svg", toShow: false, permanentShow: false},
+    {id: 1, defaultIcon:"/assets/b7.svg", icon:"/assets/android.svg", toShow: false, permanentShow: false},   
+    {id: 2, defaultIcon:"/assets/b7.svg", icon:"/assets/caminhao.svg", toShow: false, permanentShow: false},  
+    {id: 3, defaultIcon:"/assets/b7.svg", icon:"/assets/moto.svg", toShow: false, permanentShow: false},
+    {id: 4, defaultIcon:"/assets/b7.svg", icon:"/assets/disney.svg", toShow: false, permanentShow: false},   
+    {id: 5, defaultIcon:"/assets/b7.svg", icon:"/assets/estrela.svg", toShow: false, permanentShow: false},
+    {id: 6, defaultIcon:"/assets/b7.svg", icon:"/assets/android.svg", toShow: false, permanentShow: false},
+    {id: 7, defaultIcon:"/assets/b7.svg", icon:"/assets/caminhao.svg", toShow: false, permanentShow: false},
     {id: 8, defaultIcon:"/assets/b7.svg", icon:"/assets/gasolina.svg", toShow: false, permanentShow: false},
-    {id: 9, defaultIcon:"/assets/b7.svg", icon:"/assets/gasolina.svg", toShow: false, permanentShow: false},
-    {id: 10, defaultIcon:"/assets/b7.svg", icon:"/assets/moto.svg", toShow: false, permanentShow: false},
-    {id: 11, defaultIcon:"/assets/b7.svg", icon:"/assets/moto.svg", toShow: false, permanentShow: false},
+    {id: 9, defaultIcon:"/assets/b7.svg", icon:"/assets/disney.svg", toShow: false, permanentShow: false}, 
+    {id: 10, defaultIcon:"/assets/b7.svg", icon:"/assets/estrela.svg", toShow: false, permanentShow: false},
+    {id: 11, defaultIcon:"/assets/b7.svg", icon:"/assets/gasolina.svg", toShow: false, permanentShow: false},
+    
   ])
 
   const [verific, setVerific] = useState([])
@@ -94,11 +95,18 @@ const Home = () => {
 
   const handleResetButton = () => {
     setItems(prev => prev.map(item => 
-    item.toShow === true ? 
-    {...item, toShow: false}
+    item.permanentShow === true ? 
+    {...item, permanentShow: false, toShow: false}
     :
-    {...item, toShow: false}
+    {...item, permanentShow: false, toShow: false}
     ))
+    setSecond(0)
+    setMinute(0)   
+    setCount(0)
+    setMovements(0)
+    setShowCount(0)
+    setBlock(false)
+    setPlaying(false)
   }
 
   useEffect(() => {
@@ -122,14 +130,18 @@ const Home = () => {
     <div className="flex justify-center">
       <div className="container flex flex-col mx-10 xl:flex-row mt-20">
         <div className="flex-1">
- 
-          <Image
-          src="/assets/devmemory_logo.png"
-          alt=""
-          width={250}
-          height={250}
-          />
-     
+          <div className="w-80 flex">
+            <div className="w-8 h-8 grid grid-cols-2 grid rows-2 gap-1 mt-1 mx-1">
+              <div className="bg-[#1550f6]"></div>       
+              <div className="bg-[#b5bbbc]"></div>         
+              <div className="bg-[#b5bbbc] "></div>
+              <div className="bg-[#1550f6]"></div>
+            </div>
+            <div className="flex flex-col">
+              <span className="text-4xl font-bold text-[#181836]">DevMemory</span>
+              <span className="text-[#b5bbbc]">powered by Mikaessi - ib B7WEB</span>
+            </div>
+          </div>
           <div className="flex flex-col mt-10">
             <span className="text-xl text-[#b5bbbc] mb-2">Tempo</span>
             <span className="text-5xl text-[#181836] font-bold">{`0${minute}:${second < 10 ? `0${second}` : second}`}</span>
